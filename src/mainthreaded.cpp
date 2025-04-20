82668feadc37fd9a0b2e2920eaf3968437626f12
Kushal <kushal@pop-os.localdomain>
2025-04-20 14:40:11 +0530

    fixed error

--- src/mainthreaded.cpp
+++ src/mainthreaded.cpp
@@ -72,7 +72,7 @@ namespace solution {
 
                     // Micro-kernel: process the packed blocks
                     for (int ii = i; ii < i_max; ++ii) {
-                        for (int jj = j; jj < j_max; jj += 8) {
+                        for (int jj = j; jj < j_max; jj += 16) {
                             __m512 c_vec = _mm512_loadu_ps(&result[static_cast<size_t>(ii) * m + jj]);
 
                             for (int ll = kk; ll < k_max; ++ll) {
