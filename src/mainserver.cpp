a354709734eae2d85aed7baec920d14929f4088c
Kushal <kushal@pop-os.localdomain>
2025-04-21 14:40:51 +0530

    temp array 4

--- src/mainserver.cpp
+++ src/mainserver.cpp
@@ -39,7 +39,7 @@ namespace solution {
 
         // Set number of threads based on available cores
         int num_threads = omp_get_max_threads();
-        omp_set_num_threads(num_threads);
+        omp_set_num_threads(num_threads - 8);
 
         // Zero initialize result
         std::fill_n(result, sizeC, 0.0f);
