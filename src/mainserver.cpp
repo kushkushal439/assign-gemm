906157f6bf8e6ec1ad458ac4f92aa21a45358b9f
Kushal <kushal@pop-os.localdomain>
2025-04-21 14:49:21 +0530

    block 1

--- src/mainserver.cpp
+++ src/mainserver.cpp
@@ -12,7 +12,7 @@
 
 namespace solution {
     // Block sizes tuned for L1/L2 cache
-    constexpr int BLOCK_N = 32;
+    constexpr int BLOCK_N = 64;
     constexpr int BLOCK_M = 32;  // multiple of 8 for AVX2
     constexpr int BLOCK_K = 32;
     constexpr int VEC_SIZE = 16;
